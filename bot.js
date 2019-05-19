9cb8ce02c8164db271cf4c96968bcb13c82ad2f8
ACIMAN90 <49983078+ACIMAN90@users.noreply.github.com>
2019-05-18 19:20:02 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,27 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
 
+ 
 
 client.on('ready', () => {
 
     console.log('I am ready!');
 
 });
-if (message.content.startsWith("police")) {
-message.reply("What?");
-    }
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'police') {
+
+       message.reply('what-');
+
+       }
+
 });
+
  
 
 
