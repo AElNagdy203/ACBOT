83ead76ad75975c4c951dc596ad5092680cb7a34
ACIMAN90 <49983078+ACIMAN90@users.noreply.github.com>
2019-05-19 18:56:26 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,11 +69,10 @@ client.on('message', message => {
     if (message.content.startsWith("!push2")) {
     var embedWarn = new Discord.RichEmbed()
     .setTitle("► The punishments due", " ")
-    .addField("• Warn 1", " ")
-    .addField("• Warn 2", " ")
-    .addField("• Warn 3", " ")
-    .addField("• Warn 4 = Suspended", " ")
-    .addField("• Warn 6 = Ban permanently", " ")
+  .addField("• Warn 1", " ")
+  .addField("• Warn 2", " ")
+  .addField("• Warn 3", " ")
+  .addField("• Warn 6 = Ban permanently", " "
     message.channel.sendEmbed(embedWarn)
     }
 });
