fb0800307cef85837a5e6a7be5383bcda0ea9755
ACIMAN90 <49983078+ACIMAN90@users.noreply.github.com>
2020-02-26 21:15:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,8 +125,12 @@ client.on('message', message => {
 
 
 
+client.on('message', message => {
 
-
+if (message.content.startsWith("!say")) {
+message.channel.sendMessage((message.author.username) + " says: " + (message.content.replace('!say ','')));
+};
+});
 
 
 
