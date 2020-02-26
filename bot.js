ffb505eb834d891a2d77f7ca9157d083d16b58b0
ACIMAN90 <49983078+ACIMAN90@users.noreply.github.com>
2020-02-26 18:53:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -127,8 +127,8 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-if (message.content.startsWith("!staffm")) {
-message.channel.sendMessage((message.content.replace('','')));
+if (message.content.startsWith("!say")) {
+message.channel.sendMessage((message.content.replace('!say ','')));
 };
 });
 
